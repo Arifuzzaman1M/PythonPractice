b0efb373c15ddd8b4950259808c3eca38eabf735
Arifuzzaman <118435250+Arifuzzaman1M@users.noreply.github.com>
2025-04-10 14:34:55 -0700

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,18 @@
+a=int(input("enter first digit="))
+b=int(input("enter second digit="))
+
+add=a+b
+print("Total value of",a,"+",b,"is", add)
+
+multi=a*b
+print("Total multiplication value of",a,"*",b,"is", multi)
+
+sub=a-b
+print("Total sub value of",a,"-",b,"is", sub)
+
+division=a/b
+print("Total division value of",a,"/",b,"is", division)
+
+#my first python calculator which is very common and basic i know 
+#I will make complex calculator very soon
+#10/04/2025 .
